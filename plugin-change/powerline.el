d35756fcd545560c832538ba2efa8a2c6574fb1a
Конофеев Роман Русланович <roman@konofeev.ru>
2017-01-27 11:33:07 +0300

    Изменён стиль статус строки

--- plugin-change/powerline.el
+++ plugin-change/powerline.el
@@ -24,15 +24,15 @@
 
 (require 'cl-lib)
 
-(defface powerline-active1 '((t (:foreground "white" :background "#468" :inherit mode-line)))
+(defface powerline-active1 '((t (:foreground "#BCF" :background "#444455" :inherit mode-line)))
   "Powerline face 1."
   :group 'powerline)
 
-(defface powerline-active2 '((t (:foreground "white" :background "#357" :inherit mode-line)))
+(defface powerline-active2 '((t (:foreground "#ABE" :background "#3a3a4a" :inherit mode-line)))
   "Powerline face 2."
   :group 'powerline)
 
-(defface powerline-active3 '((t (:foreground "white" :background "#246" :inherit mode-line)))
+(defface powerline-active3 '((t (:foreground "#9AD" :background "#333344" :inherit mode-line)))
   "Powerline face 3."
   :group 'powerline)
 
